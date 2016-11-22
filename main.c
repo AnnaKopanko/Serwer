61a7bbdac2203a70e6795dbf72c782fc175affc6
SSeweryn <sylwia.m.seweryn@gmail.com>
2016-11-22 23:57:56 +0100

    drugi commit

--- main.c
+++ main.c
@@ -4,19 +4,18 @@
 #include <sys/socket.h>
 #include <dlfcn.h>
 
-#define BUFLEN 64  			//Max length of buffer
-#define PORT 666    		//The port on which to listen for incoming data
+#define BUFLEN 64  				//maksymalna długość bufora
+#define PORT 666    			//port, przez który wysyłamy dane
 
-struct params
-{
+struct params {
 	float a;
 	float b;
 	float (*fun)(float, float);
-}params;
+}params; //struktura z parametrami przekazywanymi przy wywolywaniu funkcji
 
 void die(char *s)
 {
-    perror(s);
+	printf(s);
     exit(1);
 }
 
@@ -26,64 +25,66 @@ int main(int argc, char *argv[])
 	uint8_t space = 0;
 	char buf_temp[BUFLEN];
 
-	// variables for dynamic library
+	//zmienne - biblioteka dynamiczna
 	void* lib_handle;
 	char* error_msg;
 
-	// variables for socket
+	//zmienne - gniazdo
 	struct sockaddr_in si_server, si_client;
     int s, slen = sizeof(si_client), recv_len;
 
-    // buffer for incoming data
+    //bufor na dane przychodzace
     char buf[BUFLEN];
 
-    float (*operation)(float, float);
+    float (*operation)(float, float); //wskaznik do funkcji
 
+    //otwarcie biblioteki dynamicznej
     lib_handle = dlopen("/home/sylwia/workspace/Serwer_C/libdlib.so", RTLD_LAZY);
 
     if (!lib_handle)
     {
-    	fprintf(stderr, "Error during dlopen(): %s\n", dlerror());
+    	fprintf(stderr, "Blad wykonywania funkcji dlopen(): %s\n", dlerror());
     	exit(1);
     }
 
-    //create a UDP socket
+    //tworzenie gniazda UDP
     if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
     {
         die("socket");
     }
 
-    // zero out the structure
+    // wypełnianie struktury si_server zerami
     memset((char *) &si_server, 0, sizeof(si_server));
 
-    si_server.sin_family = AF_INET;
-    si_server.sin_port = htons(PORT);
+    si_server.sin_family = AF_INET;		//uzycie protokolu IP
+    si_server.sin_port = htons(PORT);	//odwracanie bitow
     si_server.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    //bind socket to port
+    //nadanie gniazdu etykiety adresowej (zeby nie blokowac portu)
     if( bind(s , (struct sockaddr*)&si_server, sizeof(si_server) ) == -1)
     {
         die("bind");
     }
 
-    //keep listening for data
+    //ciągłe nasłuchiwanie na dane od klienta
     while(1)
     {
-    	printf("Waiting for data...");
+    	printf("Oczekiwanie na dane...");
         fflush(stdout);
 
-        //try to receive some data, this is a blocking call
+        //próba odbioru danych, oczekiwanie na zapytanie klienta
         if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_client, &slen)) == -1)
         {
             die("recvfrom()");
         }
 
-        //print details of the client/peer and the data received
-		printf("\nReceived packet from %s:%d\n", inet_ntoa(si_client.sin_addr), ntohs(si_client.sin_port));
-		printf("Data received: %s\n" , buf);
+        //wywietl szczegoly klienta/peer i otrzymanych danych
+		printf("\nOtrzymany pakiet od %s: %d\n", inet_ntoa(si_client.sin_addr), ntohs(si_client.sin_port));
+		printf("Otrzymane dane: %s\n" , buf);
 
 		for(i=0; i < BUFLEN; i++)
 		{
+			//pierwszy parametr programu
 			if(buf[i] == ' ')
 			{
 				space = i + 1;
@@ -93,10 +94,11 @@ int main(int argc, char *argv[])
 			buf_temp[i] = buf[i];
 		}
 
-		params.a = (float)atof(buf_temp);
+		params.a = (float)atof(buf_temp); //konwersja do float
 
 		for(i=0; i < BUFLEN;i++) buf_temp[i]='\0';
 
+		//drugi parametr programu
 		for(i = 0; i < BUFLEN; i++)
 		{
 			if(buf[space + i] == ' ')
@@ -108,10 +110,11 @@ int main(int argc, char *argv[])
 			buf_temp[i] = buf[space + i];
 		}
 
-		params.b = (float)atof(buf_temp);
+		params.b = (float)atof(buf_temp); //konwersja do float
 
 		for(i = 0; i < BUFLEN; i++) buf_temp[i]='\0';
 
+		//trzeci parametr programu
 		for(i = 0; i < BUFLEN; i++)
 		{
 			if(buf[space + i] == ' ')
@@ -122,14 +125,15 @@ int main(int argc, char *argv[])
 			buf_temp[i] = buf[space + i];
 		}
 
-		operation = dlsym(lib_handle, buf_temp);
+		////dlsym - funkcja zwraca adres
+		operation = dlsym(lib_handle, buf_temp); //do wskaznika przypisujemy funkcje na którą wskazuje nazwe operacji
 
 		error_msg = dlerror();
 		if (error_msg)
 		{
-		    sprintf(buf, "Wrong parameter of operation! Please try again.\n");
+		    sprintf(buf, "Zly parametr operacji! Sprobuj ponownie.\n");
 
-		    //now reply the client with the result
+		    //odpowiedz do klienta z wynikiem
 			if (sendto(s, buf, sizeof(buf), 0, (struct sockaddr*) &si_client, slen) == -1)
 			{
 				die("sendto()");
@@ -140,10 +144,10 @@ int main(int argc, char *argv[])
 
 		memset((char *) &buf, 0, sizeof(buf));
 
-		sprintf(buf, "Result: %.3f.\n", operation(params.a, params.b));
-		printf("Sent packet: %s", buf);
+		sprintf(buf, "Wynik: %.3f.\n", operation(params.a, params.b));
+		printf("Wyslany pakiet: %s", buf);
 
-        //now reply the client with the result
+		//odpowiedz do klienta z wynikiem
         if (sendto(s, buf, sizeof(buf), 0, (struct sockaddr*) &si_client, slen) == -1)
         {
             die("sendto()");
